1712ae2fba2ceabe2cdb7183d2b0ee47acb38d9a
kinfkong <kinfkong@126.com>
2020-08-25 06:17:02 +0800

    update

--- setup-env.sh
+++ setup-env.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-echo "running env setup..."
 
 KATAGO_BACKEND=$1
 WEIGHT_FILE=$2
 
 GPU_NAME=`nvidia-smi -q | grep "Product Name" | cut -d":" -f2 | tr -cd '[:alnum:]._-'`
-echo "Using GPU: " $GPU_NAME
-if [ "$KATAGO_BACKEND" == "AUTO" ]
-then
+#GPU_NAME=TeslaT4
+
+detect_auto_backend () {
   if [ "$GPU_NAME" == "TeslaT4" ]
   then
     KATAGO_BACKEND="CUDA"
   else
     KATAGO_BACKEND="OPENCL"
-fi
+  fi
+}
 
-if [ "$WEIGHT_FILE" == "AUTO" ]
-then
+detect_auto_weight () {
   if [ "$GPU_NAME" == "TeslaK80" ]
   then
     WEIGHT_FILE="20b"
@@ -25,22 +24,35 @@ then
     WEIGHT_FILE="20b"
   else
     WEIGHT_FILE="40b"
+  fi
+}
+
+if [ "$KATAGO_BACKEND" == "AUTO" ]
+then
+  detect_auto_backend
+fi
+
+if [ "$WEIGHT_FILE" == "AUTO" ]
+then
+  detect_auto_weight
 fi
 
 echo "Using GPU: " $GPU_NAME
 echo "Using Katago Backend: " $KATAGO_BACKEND
 echo "Using Katago Weight: " $WEIGHT_FILE
 
+
+
 cd /content
 apt install --yes libzip4 1>/dev/null
 if [ ! -d work ]
 then
-    wget https://github.com/kinfkong/ikatago-for-colab/releases/download/1.0.0/work.zip -O work.zip
-    unzip work.zip
+    wget --quiet https://github.com/kinfkong/ikatago-for-colab/releases/download/1.3.1/work.zip -O work.zip
+    unzip -qq work.zip
 fi
 
 cd /content/work
-WEIGHT_URL=`grep $WEIGHT_FILE ./weight_urls.txt | cut -d ' ' -f2`
+WEIGHT_URL=`grep "^$WEIGHT_FILE " ./weight_urls.txt | cut -d ' ' -f2`
 echo "Using Weight URL: " $WEIGHT_URL
 
 #download the binarires
