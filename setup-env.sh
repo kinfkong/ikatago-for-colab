0a2587abe9e3ba3a731acee96de963968f9b31ac
kinfkong <kinfkong@126.com>
2021-06-04 18:36:17 +0800

    fixes

--- setup-env.sh
+++ setup-env.sh
@@ -2,7 +2,8 @@
 
 KATAGO_BACKEND=$1
 WEIGHT_FILE=$2
-RELEASE_VERSION=1.4.1
+USE_HIGHTHREADS=$3
+RELEASE_VERSION=2.0.0
 GPU_NAME=`nvidia-smi -q | grep "Product Name" | cut -d":" -f2 | tr -cd '[:alnum:]._-'`
 #GPU_NAME=TeslaT4
 
@@ -50,18 +51,47 @@ wget --quiet https://github.com/kinfkong/ikatago-for-colab/releases/download/$RE
 unzip -qq work.zip
 
 cd /content/work
+wget https://github.com/kinfkong/ikatago-for-colab/releases/download/$RELEASE_VERSION/weight_urls.txt -O weight_urls.txt
 WEIGHT_URL=`grep "^$WEIGHT_FILE " ./weight_urls.txt | cut -d ' ' -f2`
 echo "Using Weight URL: " $WEIGHT_URL
 
 #download the binarires
-wget --quiet https://github.com/kinfkong/ikatago-for-colab/releases/download/$RELEASE_VERSION/katago-$KATAGO_BACKEND -O ./data/bins/katago
+if [ "$USE_HIGHTHREADS" == "TRUE" ]
+then
+wget --quiet https://github.com/kinfkong/ikatago-for-colab/releases/download/$RELEASE_VERSION/katago-highthread.zip
+unzip katago-highthread.zip
+cp ./katago-$KATAGO_BACKEND-highthread ./data/bins/katago
+else
+wget https://github.com/kinfkong/ikatago-for-colab/releases/download/$RELEASE_VERSION/katago-$KATAGO_BACKEND -O ./data/bins/katago
+fi 
 chmod +x ./data/bins/katago
+
+
 
 mkdir -p /root/.katago/
 cp -r ./opencltuning /root/.katago/
 
 #download the weights
-wget --quiet $WEIGHT_URL -O "./data/weights/"$WEIGHT_FILE".bin.gz" 
+if [[ "$WEIGHT_FILE" == "kata1"* ]]
+  then
+wget --quiet https://github.com/kinfkong/ikatago-for-colab/releases/download/$RELEASE_VERSION/kata1_weights.py -O kata1_weights.py
+python kata1_weights.py $WEIGHT_FILE
+WEIGHT_FILE="40b"
+cp /root/.katago/opencltuning/tune6_gpuTeslaK80_x19_y19_c256_mv8.txt /root/.katago/opencltuning/tune6_gpuTeslaK80_x19_y19_c256_mv10.txt
+cp /root/.katago/opencltuning/tune6_gpuTeslaP100PCIE16GB_x19_y19_c256_mv8.txt /root/.katago/opencltuning/tune6_gpuTeslaP100PCIE16GB_x19_y19_c256_mv10.txt
+cp /root/.katago/opencltuning/tune6_gpuTeslaP100PCIE16GB_x19_y19_c384_mv8.txt /root/.katago/opencltuning/tune6_gpuTeslaP100PCIE16GB_x19_y19_c384_mv10.txt
+cp /root/.katago/opencltuning/tune8_gpuTeslaK80_x19_y19_c256_mv8.txt /root/.katago/opencltuning/tune8_gpuTeslaK80_x19_y19_c256_mv10.txt
+cp /root/.katago/opencltuning/tune8_gpuTeslaP100PCIE16GB_x19_y19_c256_mv8.txt /root/.katago/opencltuning/tune8_gpuTeslaP100PCIE16GB_x19_y19_c256_mv10.txt
+elif [[ "$WEIGHT_URL" == *"zip" ]]
+  then
+  WEIGHT_FILE="40b"
+  wget --quiet $WEIGHT_URL
+  unzip g170*.zip 
+  mv ./g170*/*.bin.gz "./data/weights/"$WEIGHT_FILE".bin.gz" 
+else
+  wget --quiet $WEIGHT_URL -O "./data/weights/"$WEIGHT_FILE".bin.gz" 
+fi
+
 
 chmod +x ./change-config.sh
 ./change-config.sh $WEIGHT_FILE "./data/weights/"$WEIGHT_FILE".bin.gz"
